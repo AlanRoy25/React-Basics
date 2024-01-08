31fbc86b1a65284d6918cfeb9b3265349bbec3e8
AlanRoy25 <alanroythomas4@gmail.com>
2024-01-08 11:56:15 +0530

    creating custom react lib

--- my-vite-react/src/App.jsx
+++ my-vite-react/src/App.jsx
@@ -1,12 +1,12 @@
 import Basics from "./Basic"
 
 function App() {
-
+const username = "love or hate"
 
   return (
     <div>
         <Basics />
-     <h1>USe the values</h1>
+     <h1>USe the values username</h1>
      <p>Inside a div we can insert as many elements we want </p>
     
     </div> 
