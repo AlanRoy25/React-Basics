9487ed19a9019873d15456599ad51854ea8cf23b
AlanRoy25 <alanroythomas4@gmail.com>
2024-01-08 15:20:22 +0530

    created project based on hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+let [Counter, setCounter] = useState(15)
+  //let counterValues = 12
+
+const addValue = () =>{
+  if(Counter === 20){
+    console.log("Cant go above 20");
+  }
+else{
+  setCounter(Counter+1)
+
+}
+  }
+const removeValues = () =>{
+  
+  if(Counter ===0){
+    console.log("Can't go below negative");
+  }
+  else{
+    setCounter(Counter -1)
+  }
+}
+  return (
+    <>
+     <h1>React with Alan</h1>
+     <h2>Counter Values {Counter}</h2>
+     <button onClick={addValue}>Add Values {Counter}</button>
+     <br />
+     <button onClick={removeValues}>Remove Values {Counter}</button>
+    </>
+  ) 
+}
+
+export default App
