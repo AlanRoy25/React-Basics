f3c87d220254526549ce2706de7734250b0c3d3d
AlanRoy25 <alanroythomas4@gmail.com>
2024-04-22 23:11:01 +0530

    few updated changes

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -3,18 +3,14 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
-  let myObj = {
-    username: "Alan", 
-    Age: 22
-  }
-  let newArr = [1,2,3]
+ 
 
   return (
     <>
-    <h1 className='bg-green-400 text-black rounded-xl'>Tailwind learn with Alan</h1>
-    <Card username = "alan" btntxt = " visit me" />
-    <Card username= "ali" />
-
+   
+<Card username="MewithReact"/>
+<Card username= "Alan" />
+<Card username= "Alan" btntxt='check me out bitch' />
     </>
   )
 }
